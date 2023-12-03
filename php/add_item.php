964f584e629dd5606b6d3a57dfde842a30f0dd22
mrart <bykov.ad@students.dvfu.ru>
2023-12-03 11:31:20 +1000

    add items to db

--- php/add_item.php
+++ php/add_item.php
@@ -1,1 +1,16 @@
 <?php
+$db = new SQLite3('../db.sqlite');
+
+$pic = $_GET['pic'];
+$title = $_GET['title'];
+$desc = $_GET['desc'];
+$price = $_GET['price'];
+
+$stmt = $db->prepare("INSERT INTO Items (item_image, item_label, item_desc, item_price) VALUES (:pic, :title, :desc, :price)");
+$stmt->bindValue(':pic', $pic, SQLITE3_TEXT);
+$stmt->bindValue(':title', $title, SQLITE3_TEXT);
+$stmt->bindValue(':desc', $desc, SQLITE3_TEXT);
+$stmt->bindValue(':price', $price, SQLITE3_TEXT);
+$result = $stmt->execute();
+
+$db->close();
--- scripts/shop.js
+++ scripts/shop.js
@@ -1,6 +1,18 @@
+function goto_edit_form() {
+    window.location.href = "../pages/add_item.html"
+}
+
 function load_items(data) {
     $(document).ready(function () {
         $('#goods').html(data);
+    });
+}
+
+function add_item(pic, title, desc, price) {
+    $.ajax({
+        url: '../php/add_item.php',
+        type: 'GET',
+        data: {pic: pic, title: title, desc: desc, price: price}
     });
 }
 
@@ -9,7 +21,18 @@ $(document).ready(function () {
         url: '../php/get_items.php',
         type: 'GET',
         success: function (response) {
-            load_items(response)
+            load_items(response);
         }
+    });
+});
+
+$(document).ready(function () {
+    $('#add_item').click(function () {
+        let pic = $('#item_pic_url').val();
+        let title = $('#item_title').val();
+        let desc = $('#item_desc').val();
+        let price = $('#item_price').val();
+
+        add_item(pic, title, desc, price);
     });
 });
